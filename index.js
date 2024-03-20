79688da0aa2b51366ea4128d582533b2c8fdacf9
vishal ghosh <ghoshvishal2571@gmail.com>
2024-03-20 23:34:47 +0530

    css

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 const ul = (data) => {
-  data.map((ele) => {
-    let title = document.createElement("p")
-    title.innerHTML = ele.title
 
-    document.getElementById("box").append(title)
+  let tags=""
+  data.map((ele) => {
+    // let title = document.createElement("p")
+    // title.innerHTML = ele.title
+
+    // let images = document.createElement("img")
+    // images.src = ele.thumbnail
+    tags+= `
+          
+         <div class="items">
+        <img src=${ele.thumbnail}>
+        <h1>${ele.title}</h1>
+        
+        <p>${ele.rating}</p>
+        </div>`
+        
+        
+
   })
+  document.getElementById("box").innerHTML= tags
+
 }
 
 fetch("https://dummyjson.com/products")
   .then((res) => res.json())
-  .then((data) => ul(data.products));
+  .then((data) => ul(data.products))
 
 
